56981eb43853b9db539343c574b307ba7ee24a0f
sanjeevjayasurya <sanjeev11398@gmail.com>
2021-10-12 02:01:56 +0530

    chore: add custom font(Noto)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      noto: ['Noto Sans Mono', 'monospace'],
+    },
     extend: {},
   },
   variants: {
