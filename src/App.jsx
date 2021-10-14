ba75c565e490c52f17020d3bee9e378c9ef7fbbd
sanjeevjayasurya <sanjeev11398@gmail.com>
2021-10-14 23:20:58 +0530

    feat: add scroll-snap property

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
+import { About } from "./components/About";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <main className="font-sans bg-white w-full dark:bg-gray-800">
-      <div>
-        <Navbar />
-        <Hero />
-      </div>
+      <Navbar />
+      <Hero />
     </main>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
 export const Hero = () => {
   return (
-    <section className="min-h-screen w-full bg-white dark:bg-gray-800 px-6 md:px-24 xl:px-36">
-      <div className="flex justify-center max-w-screen-2xl flex-start h-screen flex-col lg:mx-20">
-        <p className="font-noto md:text-[16px] text-gray-800 dark:text-white font-medium text-sm mb-5">
-          Hey there, I'm
-        </p>
-        <p className="font-sans text-gray-800 dark:text-white text-4xl md:text-6xl lg:text-7xl font-bold mb-5">
-          Sanjeev JayaSurya.
-        </p>
-        <p className="font-sans text-gray-800 dark:text-white text-4xl md:text-6xl lg:text-7xl font-bold mb-5">
-          Engineering @ Uni.
-        </p>
-        <p className="font-sans text-gray-800 md:text-xl dark:text-white text-md max-w-lg">
-          I’m a software engineer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building accessible, human-centered products at Uni.
-        </p>
+    <section className="min-h-screen w-full bg-white dark:bg-gray-800 px-6 md:px-24 xl:px-36 mx-auto">
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex justify-center h-screen flex-col lg:mx-auto">
+          <p className="font-noto md:text-[16px] text-gray-800 dark:text-white font-medium text-sm lg:text-base mb-5">
+            Hey there, I'm
+          </p>
+          <p className="font-sans text-gray-800 dark:text-white text-4xl md:text-6xl lg:text-7xl font-bold mb-5">
+            Sanjeev JayaSurya.
+          </p>
+          <p className="font-sans text-gray-800 dark:text-white text-4xl md:text-6xl lg:text-7xl font-bold mb-8">
+            Engineering @ Uni.
+          </p>
+          <p className="font-sans text-gray-800 md:text-xl dark:text-white text-base lg:text-lg max-w-lg">
+            Description here
+          </p>
+        </div>
       </div>
     </section>
   );
